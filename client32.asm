c1b9e8615bbf6b91b62aa2a2773a54f752fa2488
William <wlcc@live.ca>
2019-10-18 17:46:32 -0700

    increase variable size from 5 to 10

--- client32.asm
+++ client32.asm
@@ -6,8 +6,8 @@
 ;;      TEST FILES FOR RUNNING SIMPLE FUNCTIONS
 
 SECTION .bss
-   servip1 rest 1
-   servport rest 1
+   servip1 rest 2
+   servport rest 2
 
 SECTION .data
    ip_msg1      db "Please enter the server's ip in hex: ", 0x0a, 0
