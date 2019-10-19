2b402c0711ee0e51921caed6bcc630bd652f1e48
William <wlcc@live.ca>
2019-10-18 17:59:04 -0700

    resq 2 to attempt bug fix

--- client32.asm
+++ client32.asm
@@ -6,8 +6,8 @@
 ;;      TEST FILES FOR RUNNING SIMPLE FUNCTIONS
 
 SECTION .bss
-   servip1 resw 1
-   servport resw 1
+   servip1 resq 2
+   servport resq 2
 
 SECTION .data
    ip_msg1      db "Please enter the server's ip in hex: ", 0x0a, 0
