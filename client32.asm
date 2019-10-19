69986a00f13ffd198920b4a4fe243b74bd186d31
William <wlcc@live.ca>
2019-10-18 18:44:28 -0700

    adding nasm connect functions

--- client32.asm
+++ client32.asm
@@ -30,7 +30,64 @@ _start:
 
     call f_quit
 
-
+_socket:
+ 
+    push    byte 6              ; push 6 onto the stack (IPPROTO_TCP)
+    push    byte 1              ; push 1 onto the stack (SOCK_STREAM)
+    push    byte 2              ; push 2 onto the stack (PF_INET)
+    mov     ecx, esp            ; move address of arguments into ecx
+    mov     ebx, 1              ; invoke subroutine SOCKET (1)
+    mov     eax, 102            ; invoke SYS_SOCKETCALL (kernel opcode 102)
+    int     80h                 ; call the kernel
+ret
+ 
+_connect:
+ 
+    mov     edi, eax            ; move return value of SYS_SOCKETCALL into edi (file descriptor for new socket, or -1 on error)
+    push    servip1             ; push 139.162.39.66 onto the stack IP ADDRESS (reverse byte order)
+    push    servport            ; push 80 onto stack PORT (reverse byte order)
+    push    word 2              ; push 2 dec onto stack AF_INET
+    mov     ecx, esp            ; move address of stack pointer into ecx
+    push    byte 16             ; push 16 dec onto stack (arguments length)
+    push    ecx                 ; push the address of arguments onto stack
+    push    edi                 ; push the file descriptor onto stack
+    mov     ecx, esp            ; move address of arguments into ecx
+    mov     ebx, 3              ; invoke subroutine CONNECT (3)
+    mov     eax, 102            ; invoke SYS_SOCKETCALL (kernel opcode 102)
+    int     80h                 ; call the kernel
+ret
+ 
+_write:
+ 
+    mov     edx, 43             ; move 43 dec into edx (length in bytes to write)
+    mov     ecx, request        ; move address of our request variable into ecx
+    mov     ebx, edi            ; move file descriptor into ebx (created socket file descriptor)
+    mov     eax, 4              ; invoke SYS_WRITE (kernel opcode 4)
+    int     80h                 ; call the kernel
+ret
+ 
+_read:
+ 
+    mov     edx, 1              ; number of bytes to read (we will read 1 byte at a time)
+    mov     ecx, buffer         ; move the memory address of our buffer variable into ecx
+    mov     ebx, edi            ; move edi into ebx (created socket file descriptor)
+    mov     eax, 3              ; invoke SYS_READ (kernel opcode 3)
+    int     80h                 ; call the kernel
+ 
+    cmp     eax, 0              ; if return value of SYS_READ in eax is zero, we have reached the end of the file
+    jz      _close              ; jmp to _close if we have reached the end of the file (zero flag set)
+ 
+    mov     eax, buffer         ; move the memory address of our buffer variable into eax for printing
+    call    sprint              ; call our string printing function
+    jmp     _read               ; jmp to _read
+ret
+ 
+_close:
+ 
+    mov     ebx, edi            ; move edi into ebx (connected socket file descriptor)
+    mov     eax, 6              ; invoke SYS_CLOSE (kernel opcode 6)
+    int     80h                 ; call the kernel
+ret
    
 
 _get_server:
